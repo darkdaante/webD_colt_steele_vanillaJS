b6beea7d3ae8b4fe4aa64fd91aada87214e8a3ad
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-06-09 19:24:58 +0530

    Add files via upload

---
+++ Stalker Exercise/script.js
@@ -0,0 +1,5 @@
+var x=prompt("Enter Your name if you want to live");
+var y=prompt("Enter your Last Name otherwise....");
+var z=prompt("How Old are you");
+console.log("Your Full name is "+x+" "+y);
+console.log("You are "+z+" years old");
