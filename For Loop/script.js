b5b94f13101d5103f1e3b0ef5ca5448c5ef808f9
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-06-10 06:58:46 +0530

    Add files via upload

---
+++ For Loop/script.js
@@ -0,0 +1,14 @@
+for(var i=-10;i<20;++i){
+	console.log(i);
+}
+for(var i=10;i<=40;++i){
+	console.log(i);
+}
+for(var i=300;i<334;++i){
+	console.log(i);
+}
+for(var i=5;i<=50;++i){
+	if(i%5==0 && i%3==0){
+		console.log(i);
+	}
+}
